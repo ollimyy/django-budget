8af412a69b5650c5ccee4ebbd75573b953cd4637
Olli Myyryläinen <myyrylainen.olli@gmail.com>
2023-05-28 23:04:32 +0300

    Add user login and register.

--- budget/urls.py
+++ budget/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import HomeView
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import *
 
 urlpatterns = [
     path('',HomeView.as_view()),
+
+    path('accounts/register', RegisterView.as_view()),
+    path('accounts/login', LoginView.as_view(next_page="/")),
+    path('accounts/logout', LogoutView.as_view(next_page="/")),
+
+
 ]
--- budget/views.py
+++ budget/views.py
@@ -1,5 +1,12 @@
 from django.views.generic import *
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.forms import UserCreationForm
 
 class HomeView(TemplateView):
     template_name = 'budget/home.html'
 
+class RegisterView(SuccessMessageMixin, CreateView):
+	form_class = UserCreationForm
+	template_name = "registration/register.html"
+	success_url = "/accounts/login"
+	success_message = "Registration accepted. You can now log in. "
