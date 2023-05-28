d36545e487eb69fa3f8c588eabf067c1eb0306b6
Olli Myyryläinen <myyrylainen.olli@gmail.com>
2023-05-28 22:37:40 +0300

    Add PaymentCategory and RecurringPayment models.

--- budget/models.py
+++ budget/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class PaymentCategory(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class RecurringPayment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    url = models.URLField(blank= True, null= True)
+    category = models.ForeignKey(PaymentCategory, on_delete=models.CASCADE)
