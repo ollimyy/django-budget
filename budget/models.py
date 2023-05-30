f0b73e96d7f2890b37519f047673dda1ff96ae6d
Olli Myyryläinen <myyrylainen.olli@gmail.com>
2023-05-30 19:44:17 +0300

    Add unique restriction to payment and category names. Small wording changes on payment list.

--- budget/models.py
+++ budget/models.py
@@ -2,16 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class PaymentCategory(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-
 
     def __str__(self):
         return self.name
 
+
 class RecurringPayment(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     url = models.URLField(blank=True)
     category = models.ForeignKey(
