1b7a1da55f66b4cb7b7c555e78c52b2d2cd679c1
Olli Myyryläinen <myyrylainen.olli@gmail.com>
2023-05-30 19:34:12 +0300

    Add total amount of all payments to payment list. Make category field required in payment form to prevent integrity error.

--- budget/views.py
+++ budget/views.py
@@ -1,13 +1,15 @@
-from typing import Optional
+from typing import Any, Dict, Optional
 from django.views.generic import *
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.db.models import Sum
 
 from .models import *
 
 class HomeView(TemplateView):
     template_name = 'budget/home.html'
+
 
 class RegisterView(SuccessMessageMixin, CreateView):
 	form_class = UserCreationForm
@@ -15,12 +17,24 @@ class RegisterView(SuccessMessageMixin, CreateView):
 	success_url = "/accounts/login"
 	success_message = "Registration accepted. You can now log in. "
 
+
 class RecurringPaymentListView(LoginRequiredMixin, ListView):
 	model = RecurringPayment
 
 	def get_queryset(self):
 		return RecurringPayment.objects.filter(owner__username=self.request.user)
 	
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		payments = RecurringPayment.objects.filter(owner=self.request.user)
+		
+		total_amount = payments.aggregate(total=Sum('amount'))['total']
+		if total_amount is None:
+			total_amount = 0.0
+
+		context['total_amount'] = '{:.2f}'.format(total_amount)
+		return context
+
 class RecurringPaymentCreateView(LoginRequiredMixin, CreateView):
 	model = RecurringPayment
 	fields = ['category', 'name', 'amount', 'url']
@@ -28,6 +42,7 @@ class RecurringPaymentCreateView(LoginRequiredMixin, CreateView):
 
 	def form_valid(self, form):
 		form.instance.owner = self.request.user
+
 		return super().form_valid(form)
 
 	def get_form(self, form_class=None):
@@ -35,7 +50,9 @@ class RecurringPaymentCreateView(LoginRequiredMixin, CreateView):
 		form.fields['category'].queryset = PaymentCategory.objects.filter(
             owner__username__in=[self.request.user.username, 'admin']
         )
+		form.fields['category'].required = True
 		return form
+
 
 class RecurringPaymentUpdateView(UserPassesTestMixin, UpdateView):
 	model = RecurringPayment
@@ -53,6 +70,7 @@ class RecurringPaymentUpdateView(UserPassesTestMixin, UpdateView):
         )
 		return form
 	
+
 class RecurringPaymentDeleteView(UserPassesTestMixin, DeleteView):
 	model = RecurringPayment
 	success_url = "/payments"
@@ -61,12 +79,14 @@ class RecurringPaymentDeleteView(UserPassesTestMixin, DeleteView):
 		object = self.get_object()
 		return self.request.user == object.owner
 	
+
 class PaymentCategoryListView(LoginRequiredMixin, ListView):
 	model = PaymentCategory
 
 	def get_queryset(self):
 		return PaymentCategory.objects.filter(owner__username=self.request.user)
 	
+
 class PaymentCategoryCreateView(LoginRequiredMixin, CreateView):
 	model = PaymentCategory
 	fields = ['name']
@@ -75,6 +95,7 @@ class PaymentCategoryCreateView(LoginRequiredMixin, CreateView):
 	def form_valid(self, form):
 		form.instance.owner = self.request.user
 		return super().form_valid(form)
+
 
 class PaymentCategoryUpdateView(UserPassesTestMixin, UpdateView):
 	model = PaymentCategory
@@ -85,6 +106,7 @@ class PaymentCategoryUpdateView(UserPassesTestMixin, UpdateView):
 		object = self.get_object()
 		return self.request.user == object.owner
 	
+
 class PaymentCategoryDeleteView(UserPassesTestMixin, DeleteView):
 	model = PaymentCategory
 	success_url = "/categories"
